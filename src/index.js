c043a2962d238883a3d4f7bfab2d13ceea0264be
Binyam Y. Olango <125895809+binyamolango@users.noreply.github.com>
2023-07-22 21:27:29 +0300

    Add task add and remove functionalities to the todo list app

--- src/index.js
+++ src/index.js
@@ -1,39 +1,90 @@
 import './style.css';
 
 const todoList = document.getElementById('todo-list');
-const lists = [
-  {
-    index: 1,
-    description: 'Wash clothes',
-    completed: true,
-  },
-  {
-    index: 2,
-    description: 'Cook dinner',
-    completed: false,
-  },
-  {
-    index: 3,
-    description: 'Coding challenge',
-    completed: false,
-  },
-  {
-    index: 4,
-    description: 'TAWG',
-    completed: true,
-  },
-];
+const form = document.getElementById('form');
+const taskIn = document.getElementById('taskIn');
 
-const listItemDisplay = () => {
-  lists.forEach((list) => {
-    todoList.innerHTML += `
-        <div class="task">
-            <input type="checkbox">
-            <div class="list">${list.description}</div>
-            <button class="move" type="button"></button>
-        <div>
-        `;
+const displayTask = () => {
+  const tasks = JSON.parse(localStorage.getItem('listItem')) || [];
+  if (tasks === null) return;
+
+  const sortedList = tasks.slice().sort((a, b) => a.index - b.index);
+  todoList.innerHTML = '';
+  sortedList.forEach((task) => {
+    const list = `
+        <div class="task task-${task.index}">
+            <input type="checkbox" data-btn="${task.index}">
+            <input type="text" class="list" value="${task.description}" data-desc="${task.index}">
+            <button class="move" data-remove="${task.index}"></button>
+        </div>
+    `;
+    todoList.insertAdjacentHTML('beforeend', list);
   });
 };
 
-listItemDisplay();
+const saveLocalStorage = () => {
+  const tasks = JSON.parse(localStorage.getItem('listItem')) || [];
+  const tasksItem = taskIn.value;
+  taskIn.value = '';
+  if (tasksItem === null) return;
+  const task = {
+    description: tasksItem,
+    completed: false,
+    index: tasks.length,
+  };
+  const filtered = [...tasks, task];
+  localStorage.setItem('listItem', JSON.stringify(filtered));
+};
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  saveLocalStorage();
+  displayTask();
+});
+
+todoList.addEventListener('click', (e) => {
+  const clicked = e.target.closest('.move');
+  if (!clicked) return;
+  const tasks = JSON.parse(localStorage.getItem('listItem')) || [];
+  const listNum = +clicked.dataset.remove;
+  const filtered = tasks.filter((task) => task.index !== listNum);
+  let filterOrder = [];
+  filtered.forEach((task, count) => {
+    task.index = count;
+    filterOrder = [...filterOrder, task];
+  });
+  localStorage.setItem('listItem', JSON.stringify(filterOrder));
+  displayTask();
+});
+
+todoList.addEventListener('click', (e) => {
+  const clicked = e.target.closest('.list');
+  if (!clicked) return;
+  clicked.addEventListener('keyup', () => {
+    const tasks = JSON.parse(localStorage.getItem('listItem')) || [];
+    const listNum = +clicked.dataset.desc;
+    const task = tasks.find((task) => task.index === listNum);
+    task.description = clicked.value;
+    localStorage.setItem('listItem', JSON.stringify(tasks));
+  });
+});
+
+const loadLocalStorage = () => {
+  const tasks = JSON.parse(localStorage.getItem('listItem')) || [];
+  if (tasks === null) return;
+
+  const sortedList = tasks.slice().sort((a, b) => a.index - b.index);
+  sortedList.forEach((task) => {
+    const list = `
+      <div class="task task-${task.index}">
+        <input type="checkbox" data-btn="${task.index}">
+        <input type="text" class="list" value="${task.description}" data-desc="${task.index}">
+        <button class="move" data-remove="${task.index}"></button>
+      </div>
+    `;
+
+    todoList.insertAdjacentHTML('beforeend', list);
+  });
+};
+
+loadLocalStorage();
